ef514d598e9bfc08573ac94e831d91edfc692b9f
norheim.eivind@gmail.com <norheim.eivind@gmail.com@fcc7360a-f58a-5e70-534e-9f5641c6bc2a>
2013-01-28 11:57:57 +0000

    --

--- trunk/trunk/hf_tetrode/load_tetrode_files.m
+++ trunk/trunk/hf_tetrode/load_tetrode_files.m
@@ -108,11 +108,9 @@ for isess=1:nsess
         stimfile = fullfile(pathstr, [fname, '.inp']);
         stimfiles{isess} = stimfile;
     end
-    logtime=NaN;
-    logtime_blank = NaN;
-    orients = NaN;
     try
-        [logtime, logtime_blank, orients] = get_sync(stimfile,...
+        [sync_data(isess).logtime, sync_data(isess).logtime_blank, ...
+            sync_data(isess).orients] = get_sync(stimfile,...
             't_blank', t_blank(isess),'t_delay', t_delay(isess),...
             'verbose', verbose);
     catch ME
@@ -120,10 +118,6 @@ for isess=1:nsess
         out = -1;
         return
     end
-    sync_data(isess).logtime = logtime;
-    sync_data(isess).logtime_blank = logtime_blank;
-    sync_data(isess).orients = orients;
-
 end
 
 %%%%%    Load cut data    %%%%
