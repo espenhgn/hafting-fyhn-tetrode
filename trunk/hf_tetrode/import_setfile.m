87177298c1c769e02419ad89a3ea80cf8d4ee96d
espenhgn@gmail.com <espenhgn@gmail.com@fcc7360a-f58a-5e70-534e-9f5641c6bc2a>
2013-11-28 22:38:31 +0000

    trackball reader

--- trunk/hf_tetrode/import_setfile.m
+++ trunk/hf_tetrode/import_setfile.m
@@ -7,14 +7,20 @@ function [setfile] = import_setfile(filename)
 %   The remaining will be strings.
 %
 % Example, getting field name trial_time from a .set-file:
-%   [setfile] = import_setfile('path/to/file.set')
+%   [setfile] = import_setfile('path/to/file.set');
 %   setfile.trial_time
 
 fileID = fopen(filename, 'r');
 
+if fileID == -1
+    msg = ['Could not open the set file! Make sure the filname and',...
+        'path are correct.'];
+    error('hf_tetrode:import_setfile:fopen_error', msg);
+end
+
 dataArray = textscan(fileID, '%s', 'Delimiter', '');
 
-setfile = {}
+setfile = {};
 
 for ii = 1:length(dataArray{1})
     x = strsplit(dataArray{1}{ii});
